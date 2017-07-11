f56c9ad92376d0b410c2cbd59e7876e50aaac562
Nic Bonetto <nick.bonetto@gmail.com>
2017-07-11 11:40:10 -0700

    Added google initAutoComplete function. #1

--- public/main.js
+++ public/main.js
@@ -24,3 +24,16 @@ const $views = document.querySelectorAll('.view')
 const router = new HashRouter($views)
 
 router.listen()
+
+function initAutoComplete() {
+  let searchElement = document.querySelector('#search-item')
+  let autocomplete = new google.maps.places.Autocomplete(searchElement, {
+    types: ['geocode']
+  });
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    getLatLng();
+  });
+}
+
+initAutoComplete()

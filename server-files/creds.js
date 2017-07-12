43c0e97a0912c92fc442e30ac67adeed21635999
Nic Bonetto <nick.bonetto@gmail.com>
2017-07-12 15:27:24 -0700

    Registered a route for twitter and app on get. #2

--- server-files/creds.js
+++ server-files/creds.js
@@ -2,3 +2,10 @@ process.env.CONSUMER_KEY = 'IhSP9WoLI9Cj94O1PIeulq7ZT'
 process.env.CONSUMER_SECRET = 'eYZCZjtXcpupCM9sBUd8WuY59gFmzaEkC1CLOBaYmKdI7qQXMy'
 process.env.ACCESS_TOKEN = '821171206913019904-EC9x95AGxZ1l0VaeMasTsLvLsEsoOzI'
 process.env.ACCESS_TOKEN_SECRET = 'xnCeLt5T7RJ1lkCsIKpyFNxgd2U42X5YrwaGzAjsbLfoO'
+
+module.exports = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+}
--- server-files/index.js
+++ server-files/index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
-const path = require('path')
+const twit = require('twit')
+const credentials = require('./creds')
+const twitter = new twit(credentials)
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(__dirname + './../public'))
+
+app.get('/tweets/:location', (req, res) => {
+  twitter.get('search/tweets', { q: req.params.location, result_type: 'recent', lang: 'en', count: 50 }, (err, data, response) => {
+    if (err) console.log(err)
+    res.json(data)
+  })
+})
 
 app.listen(3000)

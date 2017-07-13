ffb9c6d976c2b7bab75581635b59cbb2f152fb17
Nic Bonetto <nick.bonetto@gmail.com>
2017-07-13 11:14:36 -0700

    Hid twitter keys. #2

--- server-files/index.js
+++ server-files/index.js
@@ -1,8 +1,13 @@
+require('dotenv').config({ path: './../.env' })
 const express = require('express')
 const app = express()
 const twit = require('twit')
-const credentials = require('./creds')
-const twitter = new twit(credentials)
+const twitter = new twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+})
 
 app.use(express.static(__dirname + './../public'))
 

846e8bde92436d00a5faf7adb1d32ca52efa5fa9
Nic Bonetto <nick.bonetto@gmail.com>
2017-07-14 14:26:21 -0700

    Used sentiment to give each tweet a correlating userSentiment property. #3

--- server-files/index.js
+++ server-files/index.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: './../.env' })
 const express = require('express')
 const app = express()
 const twit = require('twit')
+const sentiment = require('sentiment')
 const twitter = new twit({
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
@@ -14,6 +15,19 @@ app.use(express.static(__dirname + './../public'))
 app.get('/tweets/:location', (req, res) => {
   twitter.get('search/tweets', { q: req.params.location, result_type: 'recent', lang: 'en', count: 50 }, (err, data, response) => {
     if (err) console.log(err)
+    data.statuses.forEach((element, index) => {
+      const twitterSentiment = sentiment(element.text)
+
+      if (twitterSentiment.score > 0) {
+        element.userSentiment = 'positive'
+      }
+      else if (twitterSentiment.score < 0) {
+        element.userSentiment = 'negative'
+      }
+      else {
+        element.userSentiment = 'neutral'
+      }
+    })
     res.json(data)
   })
 })
